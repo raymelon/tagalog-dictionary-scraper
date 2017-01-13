f1723cd05e06d84cf674731668364673cb664e5e
Raymel Francisco <franciscoraymel@gmail.com>
2017-01-13 16:12:53 +0800

    Update filter_dict.py

--- filter_dict.py
+++ filter_dict.py
@@ -1,9 +1,11 @@
 with open('tagalog_dict.txt', 'r') as dic:
-     words = dic.read().split('\n')
+     words = dic.read().split('\n') 
      
 nodup = set()
 nodup_add = nodup.add
-nodup = [w for w in words if not (w in nodup or nodup_add(w))]
+nodup = [w.lower() for w in words if not (w.lower() in nodup or nodup_add(w.lower()))]
 
-#with open('tagalog_dict.txt', 'w') as dic:
-     
+with open('tagalog_dict.txt', 'w') as dic:
+	for n in nodup:
+		print(n, file=dic)
+  
